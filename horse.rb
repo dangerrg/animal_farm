3479c85dea99a410cb9cb712721d48ebb27d0e65
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-17 22:10:37 +0200

    Create horse.rb Tasks 9

---
+++ horse.rb
@@ -0,0 +1,23 @@
+require './runs'
+require './sleeps'
+
+class Horse
+  include Runs
+  include Sleeps
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def speak!
+    'Neigh!'
+  end
+
+  def self.herd_of_horses
+    [Horse.new('Horse'), Horse.new('Horse'), Horse.new('Horse'), Horse.new('Horse')]
+  end
+end
