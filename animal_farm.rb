e2a89f2e1923bfd61474d9e378f5f498d8f723e0
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-17 23:38:04 +0200

    Create instance method how_full? Task 5

--- animal_farm.rb
+++ animal_farm.rb
@@ -17,4 +17,20 @@ class AnimalFarm
     else 'No idea for this animal.  Probably bread'
     end
   end
+
+  def how_full?
+    @animals = [] if @animals.nil?
+
+    if(@animals.length == 0)
+      "We're empty!"
+    elsif(@animals.length == 1)
+      "One lonely animal..."
+    elsif(@animals.length == 2)
+      "We've plenty of room"
+    elsif(@animals.length == 3)
+      "We're nearly full...!"
+    else
+      "No more room at the farm"
+    end
+  end
 end
