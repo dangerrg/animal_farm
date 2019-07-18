e0ac8d26ee3d54521d6f6bcbc8e74670939ccb7d
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-18 12:45:11 +0200

    Declare AVERAGE_SPEED_IN_KPH
    Define #speed_in_miles_per_hour method

--- dog.rb
+++ dog.rb
@@ -5,7 +5,13 @@ class Dog
   include Runs
   include Sleeps
 
+  AVERAGE_SPEED_IN_KPH = 29
+
   def speak!
     'Woof!'
+  end
+
+  def speed_in_miles_per_hour
+    AVERAGE_SPEED_IN_KPH * KPH_TO_MPH #We can omit Runs:: because Dog 'include Runs'
   end
 end
