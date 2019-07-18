5fdd6e6a7bad49ac1db2c4cc2b3135813f0446b0
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-18 12:36:30 +0200

    Define #speed_in_miles_per_hour method

--- horse.rb
+++ horse.rb
@@ -19,6 +19,10 @@ class Horse
     'Neigh!'
   end
 
+  def speed_in_miles_per_hour
+    AVERAGE_SPEED_IN_KPH * Runs::KPH_TO_MPH
+  end
+
   def self.herd_of_horses
     [Horse.new('Horse'), Horse.new('Horse'), Horse.new('Horse'), Horse.new('Horse')]
   end
